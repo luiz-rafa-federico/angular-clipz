6bbf37a07d3d12b0ab0f9d36c012dbe523bca725
Luiz Federico <luizrafa.federico@gmail.com>
2023-01-09 18:44:47 -0500

    firebase integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCf058WEDGHdrbOXz1_sBtAD109jGDkr9c',
+    authDomain: 'clipz-21f77.firebaseapp.com',
+    projectId: 'clipz-21f77',
+    storageBucket: 'clipz-21f77.appspot.com',
+    messagingSenderId: '597681320995',
+    appId: '1:597681320995:web:4b5880dde912af3f76ad96',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCf058WEDGHdrbOXz1_sBtAD109jGDkr9c',
+    authDomain: 'clipz-21f77.firebaseapp.com',
+    projectId: 'clipz-21f77',
+    storageBucket: 'clipz-21f77.appspot.com',
+    messagingSenderId: '597681320995',
+    appId: '1:597681320995:web:4b5880dde912af3f76ad96',
+  },
 };
 
 /*
