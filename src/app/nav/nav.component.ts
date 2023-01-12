22bad849f3ccb64668372ba07466dbf9b098032e
Luiz Federico <luizrafa.federico@gmail.com>
2023-01-12 18:45:00 -0500

    auth process concluded

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -9,11 +9,21 @@ import { RegisterService } from '../services/register.service';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  constructor(public modal: ModalService, public auth: RegisterService) {}
+  constructor(
+    public modal: ModalService,
+    public auth: RegisterService,
+    private fireAuth: AngularFireAuth
+  ) {}
 
   ngOnInit(): void {}
 
   openModal() {
     this.modal.toggleModal('auth');
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault();
+
+    await this.fireAuth.signOut();
   }
 }
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -4,7 +4,7 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { map, Observable } from 'rxjs';
+import { delay, map, Observable } from 'rxjs';
 import { IUserCredentials } from '../shared/types/user';
 
 @Injectable({
@@ -13,10 +13,13 @@ import { IUserCredentials } from '../shared/types/user';
 export class RegisterService {
   private usersCollection: AngularFirestoreCollection<IUserCredentials>;
   public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
   }
 
   async registerUser(userInfo: IUserCredentials) {
@@ -26,6 +29,8 @@ export class RegisterService {
       email,
       password as string
     );
+
+    // const token = userCredentials.user?.getIdToken();
 
     await this.usersCollection.doc(userCredentials.user?.uid).set({
       name,
