19f30374967ea804639968401336c982d0192e87
Luiz Federico <luizrafa.federico@gmail.com>
2023-01-22 16:19:53 -0500

    drag events

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { timeStamp } from 'console';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
+  isDragOver = false;
+  file: File | null = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  storeFile($evt: Event) {
+    this.isDragOver = false;
+
+    this.file = ($evt as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      // video/mp4 is a mime type (type/subtype)
+      return;
+    }
   }
-
 }
